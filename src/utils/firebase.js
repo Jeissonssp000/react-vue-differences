7ca2a6f91f239d0043b3ed263b14f0380b39cc57
Jeissonssp <jeissonssp@gmail.com>
2024-06-13 07:47:16 -0500

    fix(firebase): config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,6 +2,15 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAsWVf6zrzwHyvpDud9gwgjSKYrkPiliys",
+  authDomain: "react-vue-differences.firebaseapp.com",
+  projectId: "react-vue-differences",
+  storageBucket: "react-vue-differences.appspot.com",
+  messagingSenderId: "45443550946",
+  appId: "1:45443550946:web:1037c77c739cb5a306d8bb"
+};
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 export const auth = firebase.auth();

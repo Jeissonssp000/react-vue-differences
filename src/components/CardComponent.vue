154198b55add0d075dc0c8ae698bb1aa2f3ab258
Jeissonssp <jeissonssp@gmail.com>
2024-06-12 21:03:50 -0500

    feat(CardComponent): img "modal"

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="card vertical gap">
-    <img v-if="item.img" class="cursor" :src="item.img" :alt="item.title">
+    <div v-if="item.img" class="img cursor center">
+      <img class="transition" :src="item.img" :alt="item.title" :class="{ 'image-zoom': isActive }"
+        @click="toggleClass">
+    </div>
     <h3 class="center">{{ item.title }}</h3>
     <p v-for="(text, index) in item.description" :key="index">{{ text }}</p>
     <ul v-if="item.list">
@@ -17,14 +20,42 @@ export default {
       type: Object,
       required: true
     }
+  },
+  methods: {
+    toggleClass() {
+      this.isActive = !this.isActive;
+    }
+  },
+  data() {
+    return {
+      isActive: false
+    };
   }
 }
 </script>
 
 <style scoped>
+.img {
+  width: 100%;
+  height: 200px;
+}
+
+.image-zoom {
+  position: fixed;
+  width: 80vw;
+  height: auto;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  scale: 1.1;
+  object-fit: contain;
+}
+
 img {
   width: 100%;
-  height: 30%;
+  height: 100%;
   object-fit: cover;
 }
 </style>

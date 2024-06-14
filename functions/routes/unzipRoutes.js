d6656a12fa0b97be27f6680a9915494333f344da
Jeissonssp <jeissonssp@gmail.com>
2024-06-14 14:36:36 -0500

    fix(multer): imports after uninstall

--- functions/routes/unzipRoutes.js
+++ functions/routes/unzipRoutes.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const unzipController = require('../controllers/unzipController');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
 
-router.post('/upload', upload.single('zipFile'), unzipController.fileUploadAndUnzip);
+router.post('/upload', unzipController.fileUploadAndUnzip);
 
 module.exports = router;

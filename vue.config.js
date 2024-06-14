bc1947e7f7d69e4658ca1b638c1f9cd1a576fbc1
Jeissonssp <jeissonssp@gmail.com>
2024-06-14 16:16:26 -0500

    feat(build): public folder for build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  assetsDir: 'publicFiles',
+  outputDir: path.resolve(__dirname, 'public'),
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'publicFiles'),
+    },
+  },
 })
